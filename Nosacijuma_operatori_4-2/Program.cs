ccdb75617e5b574f8dbfde61d359decc4986dc2c
Grinchs <marcisg2345@gmail.com>
2023-10-23 23:38:37 +0300

    Add project files.

---
+++ Nosacijuma_operatori_4-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ievadiet x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.Write("Ievadiet y: ");
+        double y = double.Parse(Console.ReadLine());
+
+        if (y == 0)
+        {
+            Console.WriteLine("Dalīšana ar 0");
+        }
+        else
+        {
+            double rezultāts = x / y;
+            Console.WriteLine("Rezultāts: " + Math.Round(rezultāts, 2));
+        }
+    }
+}
